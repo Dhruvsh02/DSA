6b607bac54bccbd9cb53913fc491505d7d723ec6
Dhruvsh02 <Dhruvsharma110044@gmail.com>
2025-12-06 14:34:49 +0530

    Update

--- linked-list/odd-even-LL.py
+++ linked-list/odd-even-LL.py
@@ -2,7 +2,7 @@ class Node:
     def __init__(self,data):
         self.data = data 
         self.next = None
-
+# brute force approach : O(n) space
 def odd_even_ll(head):
     if head == None or head.next == None:
         return head
@@ -27,15 +27,32 @@ def odd_even_ll(head):
         temp = temp.next
     return head
 
+# optimaized approach : O(1) space
+
+def odd_even(head):
+    if head == None or head.next == None:
+        return head 
+    odd = head
+    even = head.next
+    evenhead = head.next
+    while even !=None and even.next != None:
+        odd.next = odd.next.next
+        even.next = even.next.next
+        odd = odd.next
+        even = even.next.next
+    odd.next = evenhead
+    return head
+
+
 def main():
     arr = [1,2,3,4,5,6,7]
     head = Node(arr[0])
     current = head
     for val in arr[1:]:
-        new_node = Node(val)
+        new_node = Node(val) 
         current.next = new_node
         current = current.next
-    head = odd_even_ll(head)
+    head = odd_even_ll(head)   
     temp = head
     while temp:
         print(temp.data,end="->")
