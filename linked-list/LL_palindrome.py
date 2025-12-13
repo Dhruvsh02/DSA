07994147e6aecfd45b411cc8a7c1f2e968eba204
Dhruvsh02 <Dhruvsharma110044@gmail.com>
2025-12-13 18:14:30 +0530

    new solution

--- linked-list/LL_palindrome.py
+++ linked-list/LL_palindrome.py
@@ -20,6 +20,38 @@ def is_palindrome_ll(head):
         temp = temp.next
     return True
 
+
+# optimal appraoch 
+
+def reverse_ll_optimal(head):
+    prev = None
+    temp = head 
+    front = None
+    while temp:
+        front = temp.next
+        temp.next = prev 
+        prev = temp
+        temp = front
+    return prev
+
+def is_palindrome_ll_optimal(head):
+    if head == None or head.next == None:
+        return True
+    slow, fast = head, head
+    while fast.next != None and fast.next.next != None:
+        slow = slow.next
+        fast = fast.next.next
+    new_head = reverse_ll_optimal(slow.next)
+    first = head
+    second = new_head
+    while second != None:
+        if first.data != second.data:
+            return False
+        first = first.next
+        second = second.next
+    slow.next = reverse_ll_optimal(new_head)
+    return True
+
 def main():
     arr = [1,2,3,2]
     head = Node(arr[0])
@@ -28,7 +60,7 @@ def main():
         new_node = Node(val)
         current.next = new_node
         current = current.next
-    if is_palindrome_ll(head):
+    if is_palindrome_ll_optimal(head):
         print("Palindrome")
     else:
         print("Not a Palindrome")
